21aa9bd19afe63eb9928d57f345ba8380222b8ee
Andreea Dan <andreea_dan_1998@yahoo.com>
2020-12-17 16:08:18 +0200

    a

--- 02/app.js
+++ 02/app.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: '`ello',
-        link :'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'
+        link :'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+        finishedLink:'<a href ="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO">not a virus</a>'
     },
     methods: {
         sayHello :function(){
