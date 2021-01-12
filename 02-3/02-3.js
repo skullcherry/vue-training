3366d5600b53c9ff17f2bdaf899e5c6399a5d438
Andreea Dan <andreea_dan_1998@yahoo.com>
2021-01-12 13:52:28 +0200

    sa

---
+++ 02-3/02-3.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:'#app',
+    data:{
+        attachRed: false,
+        color: 'green'
+    },
+
+    computed:{
+        divClasses: function(){
+            return{
+                red: this.attachRed,
+                blue: !this.attachRed
+            }
+        }
+
+    },
+
+    methods:{
+     
+    }
+
+
+});
